fd4f6e75c7c5f7e83a82a9bb193fe0e4a20c01bb
Kylian Titren <kylian.titren@gmail.com>
2025-11-28 14:56:19 +0100

    change database connection

--- config/database.ts
+++ config/database.ts
@@ -1,22 +1,13 @@
-import path from 'path';
 
 export default ({ env }) => ({
-  connection: {
-    client: env('DATABASE_CLIENT', 'postgres'),
-    connection: env('DATABASE_CLIENT') === 'postgres'
-      ? {
-          host: env('DATABASE_HOST'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          ssl: env.bool('DATABASE_SSL', true)
-            ? { rejectUnauthorized: false }
-            : false,
-        }
-      : {
-          filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-        },
-    pool: { min: 0, max: 5 },
+  postgres: {
+    connection: {
+      connectionString: env('DATABASE_URL'),
+      ssl: false,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
   },
 });
