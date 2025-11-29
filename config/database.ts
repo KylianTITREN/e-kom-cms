3abb22ff5d8174b5bd7dd65adca34616d388f155
Kylian Titren <kylian.titren@gmail.com>
2025-11-29 17:20:33 +0100

    fix database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,12 +1,22 @@
+import path from 'path';
 
 export default ({ env }) => ({
   connection: {
-    client: 'postgres',
-    connectionString: env('DATABASE_URL'),
-    ssl: false,
-  },
-  pool: {
-    min: 2,
-    max: 10,
+    client: env('DATABASE_CLIENT', 'postgres'),
+    connection: env('DATABASE_CLIENT') === 'postgres'
+      ? {
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: env.bool('DATABASE_SSL', true)
+            ? { rejectUnauthorized: false }
+            : false,
+        }
+      : {
+          filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        },
+    pool: { min: 0, max: 5 },
   },
 });
